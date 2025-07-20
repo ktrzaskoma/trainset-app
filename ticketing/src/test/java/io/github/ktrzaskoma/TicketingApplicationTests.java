dbe2ef01086fd91b197cb0422c80e302f3a0a4ee
Kacper T <kttrzaskoma@gmail.com>
2025-07-20 21:59:27 +0200

    ticketing service init

---
+++ ticketing/src/test/java/io/github/ktrzaskoma/TicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ktrzaskoma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
